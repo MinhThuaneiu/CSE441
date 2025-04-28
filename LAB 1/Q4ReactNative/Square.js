c1153ca225a8c64c53890489cbe30cf4a5079e88
MinhThuaneiu <thuan.dang.cit22@eiu.edu.vn>
2025-04-27 20:44:48 -0700

    asdxasd

---
+++ LAB 1/Q4ReactNative/Square.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text, Alert, Button} from "react-native";
+import styles from "./style";
+
+function ClickOnTheSquare(value)
+{
+    Alert.alert(value);
+}
+export default Square = ({text}) => (
+    
+);
