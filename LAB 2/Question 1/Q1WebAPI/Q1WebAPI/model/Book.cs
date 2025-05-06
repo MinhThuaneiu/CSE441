cfeac9e0ccbe77cb37b8f036e464d4b8491df63a
MinhThuaneiu <thuan.dang.cit22@eiu.edu.vn>
2025-05-06 16:07:30 -0700

    lab 2 submit

---
+++ LAB 2/Question 1/Q1WebAPI/Q1WebAPI/model/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace Q1WebAPI.model
+{
+    public class Book
+    {
+        public int bookid { get; set; }
+        public String name { get; set; }
+        public String description { get; set; }
+        public Decimal price { get; set; }
+        public String note { get; set; }
+
+    }
+}
---
+++ LAB 2/Question 1/Q1WebAPI/Q1WebAPI/model/LibraryManagementContext .cs
@@ -0,0 +1,13 @@
+﻿  using Microsoft.EntityFrameworkCore;
+  using System.Data;
+
+    namespace Q1WebAPI.model
+    {
+        public class LibraryManagementContext : DbContext
+        {
+            public LibraryManagementContext(DbContextOptions<LibraryManagementContext> options) : base(options) { }
+            public DbSet<Book> Book { get; set; }
+           
+        }
+    }
+
